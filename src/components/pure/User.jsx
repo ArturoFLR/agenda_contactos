d362381fae8990df8364a501462e015ed4b4c8b8
Arturo Lopez <jhonas29@gmail.com>
2023-02-17 13:32:56 +0100

    Botón conectar/desconectar funciona

--- src/components/pure/User.jsx
+++ src/components/pure/User.jsx
@@ -1,22 +1,28 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const User = ({ name, isConnected }) => {
 
+	const [connected, setConnection] = useState(isConnected);
+
 	// Devuelve el icono Bootstrap correcto on/off
 	function getConnectIcon () {
-		if (isConnected) {
+		if (connected) {
 			return "bi bi-toggle-on";
 		} else {
 			return "bi bi-toggle-off";
 		}
 	}
 
+	function changeConnectionStatus () {
+		setConnection(!connected);
+	}
 
 	return (
 		<div>
 			<p> {name} </p>
 			<div>
-				<i className= {getConnectIcon()}></i>
+				<i className= {getConnectIcon()} onClick={changeConnectionStatus}></i>
 				<i className="bi bi-trash"></i>
 			</div>
 		</div>
